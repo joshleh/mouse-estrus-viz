818dbfc7b38f3200fdfbd35309f4fe8849add940
Joshua Lee <joshuadmlee@gmail.com>
2025-02-20 03:50:43 -0800

    Fixed axes update and improved zoom

--- script.js
+++ script.js
@@ -1,64 +1,3 @@
-// // Load Data (Example using CSV)
-// d3.csv("data/mouse_data.csv").then(function(data) {
-    
-//     // Convert data to numeric values
-//     data.forEach(d => {
-//         d.day = +d.day;
-//         d.temp = +d.temp;
-//     });
-
-//     // Set dimensions
-//     const width = 800, height = 400;
-//     const margin = { top: 20, right: 30, bottom: 40, left: 50 };
-
-//     // Create Scales
-//     const xScale = d3.scaleLinear().domain(d3.extent(data, d => d.day)).range([margin.left, width - margin.right]);
-//     const yScale = d3.scaleLinear().domain([36, 39]).range([height - margin.bottom, margin.top]);
-
-//     // Create SVG container
-//     const svg = d3.select("#tempChart");
-
-//     // Line Generator
-//     const line = d3.line()
-//         .x(d => xScale(d.day))
-//         .y(d => yScale(d.temp));
-
-//     // Draw Line Chart
-//     svg.append("path")
-//         .datum(data)
-//         .attr("fill", "none")
-//         .attr("stroke", "steelblue")
-//         .attr("stroke-width", 2)
-//         .attr("d", line);
-
-//     // Axes
-//     svg.append("g")
-//         .attr("transform", `translate(0,${height - margin.bottom})`)
-//         .call(d3.axisBottom(xScale));
-
-//     svg.append("g")
-//         .attr("transform", `translate(${margin.left},0)`)
-//         .call(d3.axisLeft(yScale));
-
-//     // Tooltip
-//     const tooltip = d3.select("#tooltip");
-    
-//     svg.selectAll("circle")
-//         .data(data)
-//         .enter().append("circle")
-//         .attr("cx", d => xScale(d.day))
-//         .attr("cy", d => yScale(d.temp))
-//         .attr("r", 5)
-//         .attr("fill", "red")
-//         .on("mouseover", (event, d) => {
-//             tooltip.style("display", "block")
-//                    .style("left", event.pageX + "px")
-//                    .style("top", event.pageY + "px")
-//                    .html(`Day: ${d.day}<br>Temp: ${d.temp}`);
-//         })
-//         .on("mouseout", () => tooltip.style("display", "none"));
-// });
-
 // Load Data
 d3.csv("data/mouse_data.csv").then(function(data) {
     data.forEach(d => {
@@ -71,11 +10,11 @@ d3.csv("data/mouse_data.csv").then(function(data) {
     const margin = { top: 20, right: 30, bottom: 40, left: 50 };
 
     // Create Scales
-    const xScale = d3.scaleLinear()
+    let xScale = d3.scaleLinear()
         .domain(d3.extent(data, d => d.day))
         .range([margin.left, width - margin.right]);
 
-    const yScale = d3.scaleLinear()
+    let yScale = d3.scaleLinear()
         .domain([d3.min(data, d => d.temp) - 0.5, d3.max(data, d => d.temp) + 0.5])
         .range([height - margin.bottom, margin.top]);
 
@@ -100,9 +39,23 @@ d3.csv("data/mouse_data.csv").then(function(data) {
         .text(d => d)
         .attr("value", d => d);
 
+    // Create axes
+    const xAxis = svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(xScale));
+
+    const yAxis = svg.append("g")
+        .attr("class", "y-axis")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(yScale));
+
     // Function to update the chart based on selected mouse
     function updateChart(selectedMouse) {
         const filteredData = data.filter(d => d.mouse === selectedMouse);
+
+        // Reset xScale domain to the full range
+        xScale.domain(d3.extent(filteredData, d => d.day));
 
         // Update line
         svg.selectAll(".line").remove();
@@ -121,7 +74,7 @@ d3.csv("data/mouse_data.csv").then(function(data) {
             .enter().append("circle")
             .attr("cx", d => xScale(d.day))
             .attr("cy", d => yScale(d.temp))
-            .attr("r", 4)  // Reduce circle size
+            .attr("r", 4)
             .attr("fill", "red")
             .on("mouseover", (event, d) => {
                 tooltip.style("display", "block")
@@ -130,6 +83,13 @@ d3.csv("data/mouse_data.csv").then(function(data) {
                     .html(`Day: ${d.day}<br>Temp: ${d.temp}`);
             })
             .on("mouseout", () => tooltip.style("display", "none"));
+
+        // ** Re-add Axes **
+        xAxis.call(d3.axisBottom(xScale));
+        yAxis.call(d3.axisLeft(yScale));
+
+        // Reset brush selection
+        svg.select(".brush").call(brush.move, null);
     }
 
     // Default selection (first mouse)
@@ -143,19 +103,25 @@ d3.csv("data/mouse_data.csv").then(function(data) {
     // Brush & Zoom
     const brush = d3.brushX()
         .extent([[margin.left, margin.top], [width - margin.right, height - margin.bottom]])
-        .on("brush end", ({ selection }) => {
+        .on("brush end", (event) => {
+            const selection = event.selection;
             if (!selection) return;
 
+            // Convert brush selection from pixels to data domain
             const [x0, x1] = selection.map(xScale.invert);
+
+            // Update x-axis domain
             xScale.domain([x0, x1]);
 
+            // Redraw elements
             svg.select(".line").attr("d", line);
             svg.selectAll("circle")
                 .attr("cx", d => xScale(d.day))
                 .attr("cy", d => yScale(d.temp));
 
-            svg.select(".x-axis").call(d3.axisBottom(xScale));
+            xAxis.call(d3.axisBottom(xScale));
         });
 
+    // Add brush to the chart
     svg.append("g").attr("class", "brush").call(brush);
 });
